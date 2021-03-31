1e18f50a2885423b84e69254c7bca7d6b566fdff
Valentin Vorobyov <voro6yov.valentin@gmail.com>
2021-03-31 19:42:42 +0600

    Add simple api.

--- api/main.py
+++ api/main.py
@@ -1,91 +1,52 @@
+import sys
 from typing import Optional
-from pydantic import BaseModel, Field
 
-from fastapi import FastAPI, Query, Path, Body, status, Depends
-from fastapi.security import OAuth2PasswordBearer
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+
+from loguru import logger
+
+logger.add(
+    sys.stdout,
+    colorize=True,
+    format="<green>{level}</green> <level>{message}</level>",
+)
+
+app = FastAPI()
+
+
+db = {}
 
 
 class Item(BaseModel):
     name: str
-    description: Optional[str] = Field(
-        None, title="The description of the item", max_length=300
-    )
-    price: float = Field(
-        ..., gt=0, description="The price must be greater than zero"
-    )
+    description: Optional[str] = None
+    price: float
     tax: Optional[float] = None
 
-    class Config:
-        schema_extra = {
-            "example": {
-                "name": "Foo",
-                "description": "A very nice Item",
-                "price": 35.4,
-                "tax": 3.2,
-            }
-        }
 
-
-app = FastAPI()
-
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-
-
-@app.get("/")
-async def root():
-    return {"message": "hello, world!"}
-
-
-@app.get("/items/{item_id}")
-async def read_item(
-    token: str = Depends(oauth2_scheme),
-    item_id: int = Path(..., title="The ID of the item to get", gt=1),
-    q: Optional[str] = Query(None, alias="item-query"),
-    short: bool = False,
-):
-    item = {"item_id": item_id}
-    if q:
-        item.update({"q": q})
-    if not short:
-        item.update({"description": "This is an amazing item."})
-    item.update({"token": token})
+@app.get("/items/{item_name}")
+async def read_item(item_name: str):
+    logger.info(f"[Reading an item] - {item_name}")
+    item = db.get(item_name)
+    if not item:
+        logger.error(f"[Item not found] - {item_name}")
+        raise HTTPException(status_code=404, detail="Item not found")
     return item
 
 
-@app.post(
-    "/items/", response_model=Item, status_code=status.HTTP_201_CREATED
-)
-async def create_item(item: Item = Body(..., embed=True)) -> Item:
-    """
-    Creates item.
-    """
+@app.post("/items/")
+async def create_item(item: Item):
+    logger.info(f"[Creating an item] - {item.name}")
+    if item.name in db:
+        logger.error(f"[Item already exist] - {item.name}")
+        raise HTTPException(
+            status_code=403, detail="Item already exist"
+        )
     item_dict = item.dict()
     if item.tax:
+        logger.info(f"[Calculating price with tax] - tax: {item.tax}")
         price_with_tax = item.price + item.tax
         item_dict.update({"price_with_tax": price_with_tax})
+    db[item.name] = item
     return item_dict
-
-
-@app.get("/users/me")
-async def read_user_me():
-    return {"user": "the current user"}
-
-
-@app.get("/users/{user_id}")
-async def read_user(user_id: str):
-    return {"user_id": user_id}
-
-
-@app.get("/users/{user_id}/items/{item_id}")
-async def read_user_item(
-    user_id: int,
-    item_id: str,
-    q: Optional[str] = None,
-    short: bool = False,
-):
-    item = {"item_id": item_id, "owner_id": user_id}
-    if q:
-        item.update({"q": q})
-    if not short:
-        item.update({"description": "This is an amazing item."})
-    return item
